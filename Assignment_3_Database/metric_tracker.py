0c3570290b6befaf7a8e9eba4c847e3c9f60265f
DiegoOzuna <ozunad2002@gmail.com>
2024-03-19 16:49:24 -0700

    updated metric_tracker to include user ids.

--- Assignment_3_Database/metric_tracker.py
+++ Assignment_3_Database/metric_tracker.py
@@ -15,49 +15,56 @@ driver.get("http://localhost:3000/")
 
 metrics = []
 num_clicks = 0
-# Track presence time 
-start_time = time.time()
-presence_time = start_time
-while True:
-    current_time = time.time()
-    presence_time = current_time - start_time
-    print(f"Presence time: {presence_time:.2f} seconds")
-    
-    if presence_time >= 30:  #only measure for 30 seconds
-        break
 
-    # Track scrolling
-    scroll_height = driver.execute_script("return document.body.scrollHeight")  
-    current_scroll = driver.execute_script("return window.pageYOffset")
-    print(f"Scrolled {current_scroll}/{scroll_height} pixels")
-    
-    time.sleep(2) 
+# Initialize a list of user_ids. Keep it simple for now. 1 and 2 
+user_ids = [1, 2]
 
-    # Track clicks   
-    button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-    # for button in buttons:
-    button.click()
-    num_clicks += 1
-    print(f"Button was clicked: {num_clicks} times")
+for user_id in user_ids:
+    # Track presence time 
+    start_time = time.time()
+    presence_time = start_time
+    while True:
+        current_time = time.time()
+        presence_time = current_time - start_time
+        print(f"Presence time: {presence_time:.2f} seconds")
+        
+        if presence_time >= 30:  #only measure for 30 seconds
+            break
 
-    # Get title page
-    title = driver.title
-    print(f"Title page: {title}")
+        # Track scrolling
+        scroll_height = driver.execute_script("return document.body.scrollHeight")  
+        current_scroll = driver.execute_script("return window.pageYOffset")
+        print(f"Scrolled {current_scroll}/{scroll_height} pixels")
+        
+        time.sleep(2) 
 
-    # Get contents of three paragraphs based on the while loop
-    paragraph_number = num_clicks % 3 + 1
-    paragraph = driver.find_element(by=By.CSS_SELECTOR, value=f"p:nth-child({paragraph_number})")
-    print(f"Contents of paragraph {paragraph_number}: {paragraph.text}")
+        # Track clicks   
+        button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-    # Append metrics to the array
-    metrics.append({
-        "presence_time": presence_time,
-        "scroll_position": current_scroll,
-        "button_clicks": num_clicks,
-        "title_page": title,
-        "paragraph": f"paragraph {paragraph_number} :"+paragraph.text
-    })
+        # for button in buttons:
+        button.click()
+        num_clicks += 1
+        print(f"Button was clicked: {num_clicks} times")
+
+        # Get title page
+        title = driver.title
+        print(f"Title page: {title}")
+
+        # Get contents of three paragraphs based on the while loop
+        paragraph_number = num_clicks % 3 + 1
+        paragraph = driver.find_element(by=By.CSS_SELECTOR, value=f"p:nth-child({paragraph_number})")
+        print(f"Contents of paragraph {paragraph_number}: {paragraph.text}")
+
+        # Append metrics to the array
+        metrics.append({
+            "user_id": user_id,
+            "presence_time": presence_time,
+            "scroll_position": current_scroll,
+            "button_clicks": num_clicks,
+            "title_page": title,
+            "paragraph": f"paragraph {paragraph_number} :"+paragraph.text
+        })
 
 
 #grab key from hidden json file...
@@ -73,8 +80,10 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['metrics']
 
 for metric in metrics:
-    for key, value in metric.items():
-        collection = db[key]  # Get the collection for this key
-        collection.insert_one({"value": value})  # Insert the value as a new document
-        
+        for key, value in metric.items():
+            if key != "user_id":
+                collection = db[key]  # Get the collection for this key
+                # Insert the value as a new document with the user_id
+                collection.insert_one({"user_id": metric["user_id"], "value": value})
+
 driver.quit()
